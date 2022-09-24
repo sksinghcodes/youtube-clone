03bb7ef0060f3308d23d21ed0d57e3006b6af45c
SK <sksinghcodes@gmail.com>
2022-09-25 02:55:54 +0530

    Update code

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,52 @@
-const VideoCard = () => {
-    return ( <div>VideoCard</div> );
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import {
+    demoThumbnailUrl,
+    demoVideoUrl,
+    demoVideoTitle,
+    demoChannelUrl,
+    demoChannelTitle
+} from '../utils/Constants';
+
+const VideoCard = ({video}) => {
+    const { id, snippet } = video;
+    return (
+        <Card sx={{width: 358}}>
+            <Link to={`/video/${id.videoId}`}>
+                <CardMedia
+                    image={snippet?.thumbnails?.high?.url}
+                    component="img"
+                    alt=""
+                    sx={{width: 358, height: 180}}
+                />
+            </Link>
+            <CardContent
+                sx={{
+                    backgroundColor:'#1e1e1e',
+                    height: '106px',
+                }}
+            >
+                <Link to={`/video/${id.videoId}`}>
+                    <Typography
+                        variant="subtitle1"
+                        color="#fff"
+                        fontWeight="bold"
+                    >
+                        {snippet?.title?.slice(0,60)}
+                    </Typography>
+                </Link>
+
+                <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+                    <Typography variant="subtitle2" color="gray">
+                        {snippet?.channelTitle || demoChannelTitle}
+                        <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                    </Typography>
+                </Link>
+            </CardContent>
+        </Card>
+    );
 }
  
 export default VideoCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,6 @@ import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
 const Videos = ({videos}) => {
-    console.log(videos);
     return (
         <Stack
             direction="row"
@@ -10,12 +9,12 @@ const Videos = ({videos}) => {
             justifyContent="start"
             gap={2}
         >
-            {videos.map(item => (
-                <Box key={item.id.videoId || item.id.channelId}>
+            {videos.map(item => {
+                return <Box key={item.id.videoId || item.id.channelId}>
                     {item.id.videoId && <VideoCard video={item} />}
                     {item.id.channelId && <ChannelCard channelDetail={item} />}
                 </Box>
-            ))}
+            })}
         </Stack>
     );
 }
